19209d75a91ed72d00093e2eccf3ac91c3b58cf1
kreslavskiy <mishak1000236@gmail.com>
2023-02-28 21:35:21 +0200

    server.go: Added error handling and writing json data to response

--- server.go
+++ server.go
@@ -3,18 +3,26 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"time"
-    )
+)
 
 type JSONtime struct {
-  CURRENT_TIME string json:"time"
+  CURRENT_TIME string `json:"time"`
 }
 
 func getCurrentTime(w http.ResponseWriter, r *http.Request) {
   fmt.Printf("got request\n")
   currentTime := JSONtime{CURRENT_TIME: time.Now().Format(time.RFC3339)}
   json_data, err := json.Marshal(currentTime)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	io.WriteString(w, string(json_data))
 }
 
 func main() {}
