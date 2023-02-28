8175aa54e5cb8ee892e6ac97ec75b07b389d3d08
Stefan-Bilokrynytskyi <stb3002@gmail.com>
2023-02-28 21:30:12 +0200

    server.go: added time controller function

--- server.go
+++ server.go
@@ -1,7 +1,20 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+    )
+
 type JSONtime struct {
- CURRENT_TIME string json:"time"
+  CURRENT_TIME string json:"time"
+}
+
+func getCurrentTime(w http.ResponseWriter, r *http.Request) {
+  fmt.Printf("got request\n")
+  currentTime := JSONtime{CURRENT_TIME: time.Now().Format(time.RFC3339)}
+  json_data, err := json.Marshal(currentTime)
 }
 
 func main() {}
